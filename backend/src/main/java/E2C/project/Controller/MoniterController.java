48c25ca559fb8ada6e80b65c66b10e9664f3e022
ohhondgi <5ghdrl@naver.com>
2022-05-30 05:02:12 +0900

    chore: Modify Bucket & Object List

--- backend/src/main/java/E2C/project/Controller/MoniterController.java
+++ backend/src/main/java/E2C/project/Controller/MoniterController.java
@@ -1,14 +1,11 @@
 package E2C.project.Controller;
 
-import E2C.project.Service.BootService;
 import E2C.project.Service.MonitorService;
-import io.minio.Result;
-import io.minio.messages.Bucket;
-import io.minio.messages.Item;
+import E2C.project.domain.BucketDto;
+import E2C.project.domain.ObjectDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +17,14 @@ public class MoniterController {
 
     private final MonitorService monitorService;
 
-    @PostMapping("bucket-detail-page")
-    public ResponseEntity<List<Bucket>> giveBucketInformation(){
+    @GetMapping("bucket-detail-page")
+    public ResponseEntity<List<BucketDto>> giveBucketInformation(){
         return ResponseEntity.ok(monitorService.MinIOGetBucketList());
     }
 
-    @PostMapping("object-detail-page")
-    public ResponseEntity<Result<Item>> giveObjectInformation(){
-        return ResponseEntity.ok(monitorService.MinIOGetObjectList());
+    @GetMapping("object-detail-page")
+    public ResponseEntity<List<ObjectDto>> giveObjectInformation(@RequestParam String bucketName){
+        return ResponseEntity.ok(monitorService.MinIOGetObjectList(bucketName));
     }
 
 }
